a4d8d315cabf4826ee972f7157e7cdd8f970fb10
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-18 01:32:02 +0530

    Added a Helper Function for Multer which will be used to add Images
    It will also be used to handle multipart/form-data request body

---
+++ helpers/multer.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fs.mkdirSync("uploads/", { recursive: true });
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "blog" + "-" + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
