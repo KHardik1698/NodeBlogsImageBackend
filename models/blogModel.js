87550faa1817503271b57f9a284d69960cbb9ca8
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-18 01:24:29 +0530

    Created a Blog Model for Mongoose Schema
    Added Validations as well in the Schema

---
+++ models/blogModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const uniqid = require("uniqid");
+
+const blogSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: "blog-" + uniqid(),
+  },
+  author: {
+    type: String,
+    validate: {
+      validator: function (author) {
+        return this.author.trim().length !== 0;
+      },
+      message: "Author Name should not be an Empty String.",
+    },
+  },
+  title: {
+    type: String,
+    validate: {
+      validator: function (title) {
+        return this.title.trim().length !== 0;
+      },
+      message: "Blog Title should not be an Empty String.",
+    },
+  },
+  content: {
+    type: String,
+    validate: {
+      validator: function (content) {
+        return this.content.trim().length !== 0;
+      },
+      message: "Blog Content should not be an Empty String.",
+    },
+  },
+  links: {
+    type: Array,
+    default: [],
+  },
+  imageUrl: {
+    type: String,
+    validate: {
+      validator: function (imageUrl) {
+        return this.imageUrl.trim().length !== 0;
+      },
+      message: "Image was not uploaded.",
+    },
+  },
+});
+
+const BlogSchema = mongoose.model("Blog", blogSchema);
+
+module.exports = BlogSchema;
