3feb392de8bc03b4f907481f044088518cadde49
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-08 19:44:55 +0530

    Updated Express Server Startup Process
    Fixed a design flaw where the Server would start even without connecting to the Database

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const blogRoute = require("./routes/blogRoutes");
 
+app.use(express.json());
+app.use("/blogs", blogRoute);
+
 mongoose.connect(
   process.env.DATABASE_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, connection) => {
     if (err) {
-      return console.log("Error in connecting to the Database.");
+      return console.log("Error in connecting to the Database. Express Server not Started.");
     }
     console.log("Connection to the Database was Successful.");
+    app.listen(process.env.PORT, console.log(`Server has started at PORT ${process.env.PORT}`));
   }
-);
-
-app.use(express.json());
-app.use("/blogs", blogRoute);
-
-app.listen(
-  process.env.PORT,
-  console.log(`Server has started at PORT ${process.env.PORT}`)
 );
